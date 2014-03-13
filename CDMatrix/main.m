88d4197477498f30ea405b48d360894489ca7936
Carl Dong <dongcarl@yahoo.co.uk>
2014-03-13 23:24:05 +0800

    Initial Commit

---
+++ CDMatrix/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  CDMatrix
+//
+//  Created by Carl Dong on 3/13/14.
+//  Copyright (c) 2014 Carl Dong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
